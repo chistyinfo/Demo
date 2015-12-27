2519de97bb1fb17fc36758758d90b60607242d7b
chisty <chistyinfo@gmail.com>
2015-12-27 19:15:37 +0600

    Initial commit

---
+++ app/src/main/java/megaminds/demo/WelActivity.java
@@ -0,0 +1,13 @@
+package megaminds.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wel);
+    }
+}
